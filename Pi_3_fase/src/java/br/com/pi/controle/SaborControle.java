54e5f877d15e0d98229234af324253f77d1b6ded
Eduardo Moraes <lorvakchaos@gmail.com>
2012-12-06 09:51:30 -0200

    06/12/2012 09:51

--- Pi_3_fase/src/java/br/com/pi/controle/SaborControle.java
+++ Pi_3_fase/src/java/br/com/pi/controle/SaborControle.java
@@ -7,46 +7,28 @@ package br.com.pi.controle;
 import br.com.pi.dao.SaborDAO;
 import br.com.pi.dao.SaborDAOImp;
 import br.com.pi.entidade.Sabor;
+import java.util.ArrayList;
 import java.util.List;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-import javax.faces.context.FacesContext;
-import javax.faces.event.ActionEvent;
 import javax.faces.model.DataModel;
+import javax.faces.context.FacesContext;
 import javax.faces.model.ListDataModel;
+import javax.faces.model.SelectItem;
 
 /**
  *
- * @author Hugo
+ * @author Eduardo Moraes Silveira
  */
 @ManagedBean
 @SessionScoped
 public class SaborControle {
 
     private Sabor sabor;
-    private SaborDAO saborDAO;
+    private SaborDAO dao;
     private DataModel model;
-
-    public Sabor getSabor() {
-        if (sabor == null) {
-            sabor = new Sabor();
-        }
-        return sabor;
-    }
-
-    public void setSabor(Sabor sabor) {
-        this.sabor = sabor;
-    }
-
-    public SaborDAO getSaborDAO() {
-        return saborDAO;
-    }
-
-    public void setSaborDAO(SaborDAO saborDAO) {
-        this.saborDAO = saborDAO;
-    }
-
+        
     public DataModel getModel() {
         return model;
     }
@@ -55,81 +37,73 @@ public class SaborControle {
         this.model = model;
     }
 
-    public String salvar() {
-        FacesContext context = FacesContext.getCurrentInstance();
-        saborDAO = new SaborDAOImp();
-        if (sabor.getId() == null) {
-            saborDAO.salva(sabor);
-            context.addMessage(null,
-                    new FacesMessage(FacesMessage.SEVERITY_INFO,
-                    "Sabor salvo com sucesso!", ""));
-        } else {
-            saborDAO.altera(sabor);
-            context.addMessage(null,
-                    new FacesMessage(FacesMessage.SEVERITY_INFO,
-                    "Sabor alterado com sucesso!", ""));
+    public Sabor getSabor() {
+        if (sabor == null) {
+            sabor = new Sabor();
         }
-        limpar();
-        return "cadSabor";
+        return sabor;
     }
 
+    public void setSabor(Sabor usuario) {
+        this.sabor = usuario;
+    }
+    
     private void limpar() {
         sabor = null;
         model = null;
     }
-     public String limpaPesquisa() {
-        limpar();
-        return "pesqSabor";
+    
+    public String novo() {
+        sabor = new Sabor();
+        return "cadSabor.faces";
     }
-
-    public void pesquisaLike() {
-        saborDAO = new SaborDAOImp();
-        if (sabor != null) {
-            List<Sabor> sabors = saborDAO.getTodos();
-            model = new ListDataModel(sabors);
+    
+    public String pesq() {
+        limpar();
+        return "pesqSabor.faces";
+    }
+    
+    public String salvar() {
+        dao = new SaborDAOImp();
+        FacesContext context = FacesContext.getCurrentInstance();
+        if (sabor.getId() == null) {
+            dao.salva(sabor);
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Sabor Salvo Com Sucesso!", ""));
+        } else {
+            dao.altera(sabor);
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Sabor Alterado Com Sucesso!", ""));
+        }
+        limpar();
+        return "pesqSabor.faces";
+    }
+    
+    public void pesquisa() {
+        dao = new SaborDAOImp();
+        List<Sabor> sabores = dao.getTodos();
+        model = new ListDataModel(sabores);
+        FacesContext context = FacesContext.getCurrentInstance();
+        if (sabores.isEmpty()) {
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Sabor inesistente!", "Sabor inesistente!"));
+            limpar();
         }
     }
-
-    public String voltar() {
-        limpar();
-        return "index.feces";
-    }
-
-    public String novoSabor() {
-        sabor = new Sabor();
-
-        return "cadSabor";
-    }
-
-    public void excluir(ActionEvent evento) {
+    
+    public String excluir() {
         FacesContext context = FacesContext.getCurrentInstance();
         try {
-            saborDAO = new SaborDAOImp();
+            dao = new SaborDAOImp();
             sabor = (Sabor) model.getRowData();
-            saborDAO.remove(sabor);
-
-            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,
-                    "Sabor excluído com sucesso!", ""));
+            dao.remove(sabor);
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Sabor Excluido com sucesso!", ""));
         } catch (Exception e) {
-            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,
-                    "não foi possivel exclusão!", ""));
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "não foi posivel executar a exclusão!", ""));
         }
         limpar();
+        return "";
     }
-
+    
     public String alterar() {
-        FacesContext context = FacesContext.getCurrentInstance();
         sabor = (Sabor) model.getRowData();
-        setSabor(sabor);
-        context.addMessage(null,
-                new FacesMessage(FacesMessage.SEVERITY_INFO,
-                "Sabor alterado com sucesso!", ""));
-        return "cadSabor";
-
-    }
-
-    public String btPesquisar() {
-        sabor = null;
-        return "pesqSabor";
+        return "cadSabor.faces";
     }
 }
