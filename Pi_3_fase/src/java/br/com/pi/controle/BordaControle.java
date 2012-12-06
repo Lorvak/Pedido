f63d114a7e720c7170a01cf75c89a8cc2c2fe895
Eduardo Moraes <lorvakchaos@gmail.com>
2012-12-06 11:49:15 -0200

    06/12/2012 11:49

--- Pi_3_fase/src/java/br/com/pi/controle/BordaControle.java
+++ Pi_3_fase/src/java/br/com/pi/controle/BordaControle.java
@@ -7,46 +7,28 @@ package br.com.pi.controle;
 import br.com.pi.dao.BordaDAO;
 import br.com.pi.dao.BordaDAOImp;
 import br.com.pi.entidade.Borda;
+import java.util.ArrayList;
 import java.util.List;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-import javax.faces.context.FacesContext;
-import javax.faces.event.ActionEvent;
 import javax.faces.model.DataModel;
+import javax.faces.context.FacesContext;
 import javax.faces.model.ListDataModel;
+import javax.faces.model.SelectItem;
 
 /**
  *
- * @author Hugo
+ * @author Eduardo Moraes Silveira
  */
 @ManagedBean
 @SessionScoped
 public class BordaControle {
 
     private Borda borda;
-    private BordaDAO bordaDAO;
+    private BordaDAO dao;
     private DataModel model;
-
-    public Borda getBorda() {
-        if (borda == null) {
-            borda = new Borda();
-        }
-        return borda;
-    }
-
-    public void setBorda(Borda borda) {
-        this.borda = borda;
-    }
-
-    public BordaDAO getBordaDAO() {
-        return bordaDAO;
-    }
-
-    public void setBordaDAO(BordaDAO bordaDAO) {
-        this.bordaDAO = bordaDAO;
-    }
-
+        
     public DataModel getModel() {
         return model;
     }
@@ -55,81 +37,73 @@ public class BordaControle {
         this.model = model;
     }
 
-    public String salvar() {
-        FacesContext context = FacesContext.getCurrentInstance();
-        bordaDAO = new BordaDAOImp();
-        if (borda.getId() == null) {
-            bordaDAO.salva(borda);
-            context.addMessage(null,
-                    new FacesMessage(FacesMessage.SEVERITY_INFO,
-                    "Borda salvo com sucesso!", ""));
-        } else {
-            bordaDAO.altera(borda);
-            context.addMessage(null,
-                    new FacesMessage(FacesMessage.SEVERITY_INFO,
-                    "Borda alterado com sucesso!", ""));
+    public Borda getBorda() {
+        if (borda == null) {
+            borda = new Borda();
         }
-        limpar();
-        return "cadBorda";
+        return borda;
     }
 
+    public void setBorda(Borda usuario) {
+        this.borda = usuario;
+    }
+    
     private void limpar() {
         borda = null;
         model = null;
     }
-     public String limpaPesquisa() {
-        limpar();
-        return "pesqBorda";
+    
+    public String novo() {
+        borda = new Borda();
+        return "cadBorda.faces";
     }
-
-    public void pesquisaLike() {
-        bordaDAO = new BordaDAOImp();
-        if (borda != null) {
-            List<Borda> bordas = bordaDAO.getTodos();
-            model = new ListDataModel(bordas);
+    
+    public String pesq() {
+        limpar();
+        return "pesqBorda.faces";
+    }
+    
+    public String salvar() {
+        dao = new BordaDAOImp();
+        FacesContext context = FacesContext.getCurrentInstance();
+        if (borda.getId() == null) {
+            dao.salva(borda);
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Borda Salvo Com Sucesso!", ""));
+        } else {
+            dao.altera(borda);
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Borda Alterado Com Sucesso!", ""));
+        }
+        limpar();
+        return "pesqBorda.faces";
+    }
+    
+    public void pesquisa() {
+        dao = new BordaDAOImp();
+        List<Borda> bordaes = dao.getTodos();
+        model = new ListDataModel(bordaes);
+        FacesContext context = FacesContext.getCurrentInstance();
+        if (bordaes.isEmpty()) {
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Borda inesistente!", "Borda inesistente!"));
+            limpar();
         }
     }
-
-    public String voltar() {
-        limpar();
-        return "index.feces";
-    }
-
-    public String novoBorda() {
-        borda = new Borda();
-
-        return "cadBorda";
-    }
-
-    public void excluir(ActionEvent evento) {
+    
+    public String excluir() {
         FacesContext context = FacesContext.getCurrentInstance();
         try {
-            bordaDAO = new BordaDAOImp();
+            dao = new BordaDAOImp();
             borda = (Borda) model.getRowData();
-            bordaDAO.remove(borda);
-
-            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,
-                    "Borda excluído com sucesso!", ""));
+            dao.remove(borda);
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Borda Excluido com sucesso!", ""));
         } catch (Exception e) {
-            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,
-                    "não foi possivel exclusão!", ""));
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "não foi posivel executar a exclusão!", ""));
         }
         limpar();
+        return "";
     }
-
+    
     public String alterar() {
-        FacesContext context = FacesContext.getCurrentInstance();
         borda = (Borda) model.getRowData();
-        setBorda(borda);
-        context.addMessage(null,
-                new FacesMessage(FacesMessage.SEVERITY_INFO,
-                "Borda alterado com sucesso!", ""));
-        return "cadBorda";
-
-    }
-
-    public String btPesquisar() {
-        borda = null;
-        return "pesqBorda";
+        return "cadBordab.faces";
     }
 }
--- Pi_3_fase/src/java/br/com/pi/controle/PedidoTeleControle.java
+++ Pi_3_fase/src/java/br/com/pi/controle/PedidoTeleControle.java
@@ -4,49 +4,44 @@
  */
 package br.com.pi.controle;
 
-import br.com.pi.dao.PedidoDAO;
-import br.com.pi.dao.PedidoDAOImp;
-import br.com.pi.entidade.Pedido;
+import br.com.pi.dao.PedidoTeleDAO;
+import br.com.pi.dao.PedidoTeleDAOImp;
+import br.com.pi.dao.PaisDAO;
+import br.com.pi.dao.PaisDAOImp;
+import br.com.pi.entidade.PedidoTele;
+import br.com.pi.entidade.Pais;
+import java.util.ArrayList;
 import java.util.List;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-import javax.faces.context.FacesContext;
-import javax.faces.event.ActionEvent;
 import javax.faces.model.DataModel;
+import javax.faces.context.FacesContext;
 import javax.faces.model.ListDataModel;
+import javax.faces.model.SelectItem;
 
 /**
  *
- * @author Hugo
+ * @author tecnicom
  */
 @ManagedBean
 @SessionScoped
 public class PedidoTeleControle {
 
-    private Pedido pedido;
-    private PedidoDAO pedidoDAO;
+    private PedidoTele pedidoTele;
+    private Pais pais;
+    private PedidoTeleDAO dao;
     private DataModel model;
+    private String nome;
 
-    public Pedido getPedido() {
-        if (pedido == null) {
-            pedido = new Pedido();
-        }
-        return pedido;
+    public String getNome() {
+        return nome;
     }
 
-    public void setPedido(Pedido pedido) {
-        this.pedido = pedido;
+    public void setNome(String nome) {
+        this.nome = nome;
     }
-
-    public PedidoDAO getPedidoDAO() {
-        return pedidoDAO;
-    }
-
-    public void setPedidoDAO(PedidoDAO pedidoDAO) {
-        this.pedidoDAO = pedidoDAO;
-    }
-
+    
     public DataModel getModel() {
         return model;
     }
@@ -55,81 +50,100 @@ public class PedidoTeleControle {
         this.model = model;
     }
 
-    public String salvar() {
-        FacesContext context = FacesContext.getCurrentInstance();
-        pedidoDAO = new PedidoDAOImp();
-        if (pedido.getId() == null) {
-            pedidoDAO.salva(pedido);
-            context.addMessage(null,
-                    new FacesMessage(FacesMessage.SEVERITY_INFO,
-                    "Pedido salvo com sucesso!", ""));
-        } else {
-            pedidoDAO.altera(pedido);
-            context.addMessage(null,
-                    new FacesMessage(FacesMessage.SEVERITY_INFO,
-                    "Pedido alterado com sucesso!", ""));
+    public Pais getPais() {
+        if (pais == null) {
+            pais = new Pais();
         }
-        limpar();
-        return "cadPedido";
+        return pais;
     }
 
+    public void setPais(Pais pais) {
+        this.pais = pais;
+    }
+
+    public PedidoTele getPedidoTele() {
+        return pedidoTele;
+    }
+
+    public void setPedidoTele(PedidoTele usuario) {
+        this.pedidoTele = usuario;
+    }
+    
     private void limpar() {
-        pedido = null;
+        pedidoTele = null;
+        pais = null;
         model = null;
     }
-     public String limpaPesquisa() {
-        limpar();
-        return "pesqPedido";
+    
+    public String novo() {
+        pedidoTele = new PedidoTele();
+        pais = new Pais();
+        return "cadPedidoTele.faces";
     }
-
+    
+    public String pesq() {
+        limpar();
+        return "pesqPedidoTele.faces";
+    }
+    
+    public String salvar() {
+        dao = new PedidoTeleDAOImp();
+        FacesContext context = FacesContext.getCurrentInstance();
+        if (pedidoTele.getId() == null) {
+            dao.salva(pedidoTele);
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "PedidoTele Salvo Com Sucesso!", ""));
+        } else {
+            dao.altera(pedidoTele);
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "PedidoTele Alterado Com Sucesso!", ""));
+        }
+        limpar();
+        return "pesqPedidoTele.faces";
+    }
+    
     public void pesquisaLike() {
-        pedidoDAO = new PedidoDAOImp();
-        if (pedido != null) {
-            List<Pedido> pedidos = pedidoDAO.getTodos();
-            model = new ListDataModel(pedidos);
+        dao = new PedidoTeleDAOImp();
+        List<PedidoTele> pedidoTelees = new ArrayList();
+        model = new ListDataModel(pedidoTelees);
+        FacesContext context = FacesContext.getCurrentInstance();
+        if (pedidoTelees.isEmpty()) {
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, null, "PedidoTele inesistente!"));
+            limpar();
         }
     }
-
-    public String voltar() {
-        limpar();
-        return "index.feces";
-    }
-
-    public String novoPedido() {
-        pedido = new Pedido();
-
-        return "cadPedido";
-    }
-
-    public void excluir(ActionEvent evento) {
+    
+    public String excluir() {
         FacesContext context = FacesContext.getCurrentInstance();
         try {
-            pedidoDAO = new PedidoDAOImp();
-            pedido = (Pedido) model.getRowData();
-            pedidoDAO.remove(pedido);
-
-            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,
-                    "Pedido excluído com sucesso!", ""));
+            dao = new PedidoTeleDAOImp();
+            pedidoTele = (PedidoTele) model.getRowData();
+            dao.remove(pedidoTele);
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "PedidoTele Excluido com sucesso!", ""));
         } catch (Exception e) {
-            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,
-                    "não foi possivel exclusão!", ""));
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "não foi posivel executar a exclusão!", ""));
         }
         limpar();
+        return "";
     }
-
+    
+    public String paginapesq() {
+        limpar();
+        return "pesPedidoTele.faces";
+    }
+    
     public String alterar() {
-        FacesContext context = FacesContext.getCurrentInstance();
-        pedido = (Pedido) model.getRowData();
-        setPedido(pedido);
-        context.addMessage(null,
-                new FacesMessage(FacesMessage.SEVERITY_INFO,
-                "Pedido alterado com sucesso!", ""));
-        return "cadPedido";
-
+        pedidoTele = (PedidoTele) model.getRowData();
+//        pais = pedidoTele.getPais();
+        return "cadPedidoTele.faces";
     }
-
-    public String btPesquisar() {
-        pedido = null;
-        return "pesqPedido";
+    
+    public List<SelectItem> getComboPais(){
+        PaisDAO pdao = new PaisDAOImp();
+        List<Pais> paises = pdao.getTodos();
+        List<SelectItem> listaCombo = new ArrayList<SelectItem>();
+        for (Pais forn : paises) {
+            listaCombo.add(new SelectItem(forn.getId(), forn.getNome()));
+        }
+        return listaCombo;
     }
+    
 }
