26fa8c11a5fcdabc8c1edbe2e72efff3fd649cc1
Hugo <hugo150980@gmail.com>
2013-02-18 10:46:35 -0300

    18/02/2013 10:46

--- Pi_3_fase/src/java/br/com/pi/webServices/PedidoWebService.java
+++ Pi_3_fase/src/java/br/com/pi/webServices/PedidoWebService.java
@@ -4,6 +4,9 @@
  */
 package br.com.pi.webServices;
 
+import br.com.pi.dao.PedidoDAO;
+import br.com.pi.dao.PedidoDAOImp;
+import br.com.pi.entidade.Pedido;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
@@ -17,7 +20,8 @@ public class PedidoWebService {
 
     
     @WebMethod(operationName = "hello")
-    public String hello(@WebParam(name = "name") String txt) {
-        return "Hello " + txt + " !";
+    public void hello(@WebParam(name = "name") Pedido txt) {
+        PedidoDAO pedidoDAO = new PedidoDAOImp();
+        pedidoDAO.salva(txt);
     }
 }
